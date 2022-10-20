8ca5ad63e192d716264c1507e8146ef57b8c5643
ChrisTitusTech <dfm.titus@gmail.com>
2022-10-20 10:25:00 -0500

    fix syntax

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Initial Setup file for new systems
-gitpath=`pwd`
+gitpath=$(pwd)
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
 rm -rf ~/.cache/nvim
-mkdir -p $HOME/.vim/undodir
-mkdir -p $HOME/.scripts
-ln -s $gitpath/.ignore $HOME/.ignore
-ln -s $gitpath/tinypng $HOME/.scripts/tinypng
-cp uca.xml $HOME/.config/Thunar/
-git clone https://github.com/NvChad/NvChad $HOME/.config/nvim --depth 1
-ln -s $gitpath/custom $HOME/.config/nvim/lua/custom
+mkdir -p "$HOME/.vim/undodir"
+mkdir -p "$HOME/.scripts"
+ln -s "$gitpath/.ignore" "$HOME/.ignore"
+ln -s "$gitpath/tinypng" "$HOME/.scripts/tinypng"
+cp uca.xml "$HOME/.config/Thunar/"
+git clone https://github.com/NvChad/NvChad "$HOME/.config/nvim" --depth 1
+ln -s "$gitpath/custom" "$HOME/.config/nvim/lua/custom"
 # Share system clipboard with unnamedplus
-sudo apt install vim-gtk3 ripgrep fd-find xclip neovim python3-venv luarocks go luarocks go
+sudo apt install vim-gtk3 ripgrep fd-find xclip neovim python3-venv luarocks golang-go
