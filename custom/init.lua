b5c3e0b71954f31c00361281e1c8252aa8057f06
Chris Titus <dfm.titus@gmail.com>
2022-10-26 23:21:01 -0500

    minor edits

--- custom/init.lua
+++ custom/init.lua
@@ -66,9 +66,7 @@ require'clipboard-image'.setup {
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = {
-    'awk_ls',
     'bashls',
-    'grammarly',
     'luau_lsp',
     'marksman',
     'powershell_es',
@@ -117,7 +115,7 @@ require('telescope').load_extension('projects')
 -- Smarter Indent setup
 vim.opt.list = true
 -- vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
+-- vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
     space_char_blankline = " ",
