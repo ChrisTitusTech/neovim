01cfca5bbbaee7d9bfa1aff763ab1946b805ac58
Chris Titus <dfm.titus@gmail.com>
2022-10-30 12:25:04 -0500

    fix windows errors

--- custom/init.lua
+++ custom/init.lua
@@ -27,6 +27,19 @@ vim.cmd([[
 set spell
 ]])
 
+---
+-- Suppress errors in Windows
+-- 
+
+vim.notify = function (msg, log_level, _opts)
+    if msg:match("exit code") then return end
+    if log_level == vim.log.levels.ERROR then
+        vim.api.nvim_err_writeln(msg)
+    else
+    vim.api.nvim_echo({{msg}}, true, {})
+    end
+  end
+
 -- ========================================================================== --
 -- ==                         PLUGIN CONFIGURATION                         == --
 -- ========================================================================== --
--- winsetup.ps1
+++ winsetup.ps1
@@ -9,7 +9,7 @@ irm "https://osdn.net/frs/redir.php?m=rwthaachen&f=mingw%2F68260%2Fmingw-get-set
 # Share system clipboard with unnamedplus
 #sudo apt install vim-gtk3 ripgrep fd-find xclip neovim python3-venv luarocks go luarocks go
 New-Item -Path "$HOME\AppData\Local\nvim" -ItemType SymbolicLink -Target "$pwd\custom"
-# Grab dependancies
+# Grab dependencies
 
 # Install Choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
