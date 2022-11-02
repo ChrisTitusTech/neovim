53c4d637cc1002e62e27ae64ec5d5f2caf9c3ba1
Chris Titus <dfm.titus@gmail.com>
2022-11-02 09:05:22 -0500

    fixes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # Initial Setup file for new systems
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 gitpath=$(pwd)
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
@@ -14,7 +14,3 @@ cp uca.xml "$HOME/.config/Thunar/"
 ln -s "$gitpath/custom" "$HOME/.config/nvim"
 # Share system clipboard with unnamedplus
 sudo apt install vim-gtk3 ripgrep fd-find xclip neovim python3-venv luarocks golang-go shellcheck
-# vale smart text writing https://vale.sh/docs/vale-cli/installation/
-nix-env -iA nixpkgs.vale
-cp "$gitpath/.vale.ini" "$HOME/.vale.ini"
-vale sync
