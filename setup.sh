ee84e8511dcedf1569ed2dc702750614c809eafb
ChrisTitusTech <dfm.titus@gmail.com>
2023-09-04 23:08:14 -0500

    kickstart addition

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Initial Setup file for new systems
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 gitpath=$(pwd)
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
@@ -9,8 +8,6 @@ rm -rf ~/.cache/nvim
 mkdir -p "$HOME/.vim/undodir"
 mkdir -p "$HOME/.scripts"
 cp "$gitpath/.ignore" "$HOME/.ignore"
-cp "$gitpath/tinypng" "$HOME/.scripts/tinypng"
-cp uca.xml "$HOME/.config/Thunar/"
-ln -s "$gitpath/custom" "$HOME/.config/nvim"
+ln -s "$gitpath/kickstart.nvim" "$HOME/.config/nvim"
 # Share system clipboard with unnamedplus
 sudo apt install vim-gtk3 ripgrep fd-find xclip neovim python3-venv luarocks golang-go shellcheck -y
