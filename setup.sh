a1e3255a1f43b67c13c51912dd207ab6de65671b
ChrisTitusTech <dfm.titus@gmail.com>
2023-09-04 23:38:09 -0500

    new neovim config

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ rm -rf ~/.cache/nvim
 mkdir -p "$HOME/.vim/undodir"
 mkdir -p "$HOME/.scripts"
 cp "$gitpath/.ignore" "$HOME/.ignore"
-ln -s "$gitpath/kickstart.nvim" "$HOME/.config/nvim"
+ln -s "$gitpath/titus-kickstart" "$HOME/.config/nvim"
 # Share system clipboard with unnamedplus
 sudo apt install vim-gtk3 ripgrep fd-find xclip neovim python3-venv luarocks golang-go shellcheck -y
--- winsetup.ps1
+++ winsetup.ps1
@@ -3,10 +3,4 @@ rm -r ~/AppData/Local/nvim
 rm -r ~/AppData/Local/nvim-data
 mkdir -p $HOME/.vim/undodir
 mkdir -p $HOME/.scripts
-New-Item -Path "$HOME\AppData\Local\nvim" -ItemType SymbolicLink -Target "$pwd\custom"
-
-# Install Choco
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-
-#
+New-Item -Path "$HOME\AppData\Local\nvim" -ItemType SymbolicLink -Target "$pwd\titus-kickstart"
