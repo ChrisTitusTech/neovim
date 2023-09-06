d6ad8e68807c9a590700fa220690d5233844d2c1
Chris Titus <contact@christitus.com>
2023-09-05 21:16:56 -0500

    update windows

--- winsetup.ps1
+++ winsetup.ps1
@@ -1,6 +1,6 @@
 # Initial Setup file for new systems
-rm -r ~/AppData/Local/nvim
-rm -r ~/AppData/Local/nvim-data
+Remove-Item -Recurse -Force ~/AppData/Local/nvim
+Remove-Item -Recurse -Force ~/AppData/Local/nvim-data
 mkdir -p $HOME/.vim/undodir
 mkdir -p $HOME/.scripts
 New-Item -Path "$HOME\AppData\Local\nvim" -ItemType SymbolicLink -Target "$pwd\titus-kickstart"
