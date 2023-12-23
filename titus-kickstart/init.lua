6e8fe04bd1c444b88f21d48b579900c2a969dd92
Chris Titus <dfm.titus@gmail.com>
2023-12-23 13:18:06 -0600

    fix annoying recover swap popups

--- titus-kickstart/init.lua
+++ titus-kickstart/init.lua
@@ -544,3 +544,34 @@ require 'synthwave84'.setup({
 })
 
 vim.cmd[[colorscheme synthwave84]]
+
+
+function delete_file_swaps()
+    vim.cmd('write')
+    local output = vim.fn.systemlist('sw')
+
+    if #output > 0 then
+        local current_swap_file = output[1]
+        local base = current_swap_file:gsub('%.%w+$', '')
+        local swap_files = vim.fn.glob(base .. '.s*')
+
+        -- Delete all swap files except the current one
+        local swap_files_list = vim.fn.split(swap_files, '\n')
+        for _, swap_file in ipairs(swap_files_list) do
+            if vim.fn.empty(vim.fn.glob(swap_file)) == 0 and swap_file ~= current_swap_file then
+                vim.fn.delete(swap_file)
+                print("Swap file removed: " .. swap_file)
+            end
+        end
+
+        -- Reset swap file extension to `.swp`
+        vim.cmd('set swf!')
+        vim.cmd('set swf!')
+        print("Reset swap file extension for file: " .. vim.fn.expand('%'))
+    end
+end
+
+vim.cmd('command! DeleteFileSwaps :lua delete_file_swaps()')
+
+vim.o.shortmess = vim.o.shortmess .. 'A'
+vim.o.noshortname = true
