4f1929f9dcab966076c40049fe5dc8610acc3da5
Chris Titus <dfm.titus@gmail.com>
2023-12-27 17:53:51 -0600

    zoom addition

--- titus-kickstart/init.lua
+++ titus-kickstart/init.lua
@@ -575,3 +575,32 @@ vim.cmd('command! DeleteFileSwaps :lua delete_file_swaps()')
 
 vim.o.shortmess = vim.o.shortmess .. 'A'
 vim.o.noshortname = true
+
+vim.g.gui_font_default_size = 12
+vim.g.gui_font_size = vim.g.gui_font_default_size
+vim.g.gui_font_face = "Fira Code Retina"
+
+RefreshGuiFont = function()
+  vim.opt.guifont = string.format("%s:h%s",vim.g.gui_font_face, vim.g.gui_font_size)
+end
+
+ResizeGuiFont = function(delta)
+  vim.g.gui_font_size = vim.g.gui_font_size + delta
+  RefreshGuiFont()
+end
+
+ResetGuiFont = function ()
+  vim.g.gui_font_size = vim.g.gui_font_default_size
+  RefreshGuiFont()
+end
+
+-- Call function on startup to set default value
+ResetGuiFont()
+
+-- Keymaps
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set({'n', 'i'}, "<C-+>", function() ResizeGuiFont(1)  end, opts)
+vim.keymap.set({'n', 'i'}, "<C-->", function() ResizeGuiFont(-1) end, opts)
+vim.keymap.set({'n', 'i'}, "<C-BS>", function() ResetGuiFont() end, opts)
