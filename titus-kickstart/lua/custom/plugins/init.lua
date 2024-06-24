3320c93e4a790ba696a30f88565efa2d9c2262a8
URD0TH <urd0th@gmail.com>
2024-06-24 01:20:00 -0400

    delete plugin vim-wakatime

--- titus-kickstart/lua/custom/plugins/init.lua
+++ titus-kickstart/lua/custom/plugins/init.lua
@@ -28,7 +28,6 @@ return {
 -- Titus Custom
   "postfen/clipboard-image.nvim",
   "mbbill/undotree",
-  "wakatime/vim-wakatime",
   "Pocco81/auto-save.nvim",
   "Pocco81/true-zen.nvim",
   "lambdalisue/suda.vim",
