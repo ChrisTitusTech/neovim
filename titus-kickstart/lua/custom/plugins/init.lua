dc0693e6fdb61828edc746c7ac1b547c5a748837
Chris Titus Tech <contact@christitus.com>
2026-04-15 22:44:39 -0500

    Refactor keymaps and enhance buffer/tab navigation; add new plugins for improved functionality

--- titus-kickstart/lua/custom/plugins/init.lua
+++ titus-kickstart/lua/custom/plugins/init.lua
@@ -1,10 +1,278 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
+-- All custom plugins. Simple/minimal-config plugins are at the top.
+-- Plugins with complex configuration live under [[ Advanced Configuration ]] below.
 return {
-  -- File Explorer I'll force you to use oil or use mini.files, if you desperatly want an tree viewer just use Snacks.explorer().
+
+  -- ============================================================
+  -- [[ Simple Plugins ]]
+  -- ============================================================
+  'NMAC427/guess-indent.nvim',     -- auto-detect tabstop/shiftwidth
+  'lunarvim/synthwave84.nvim',     -- alternate colorscheme
+  'emacs-grammarly/lsp-grammarly', -- Grammarly LSP for prose
+  'mbbill/undotree',               -- persistent undo history tree (mapped to <F5>)
+  'wakatime/vim-wakatime',         -- WakaTime coding time tracker
+  'lambdalisue/suda.vim',          -- edit files as sudo (:SudaWrite)
+  'ionide/Ionide-vim',             -- F# language support
+  { 'mg979/vim-visual-multi', branch = 'master' }, -- multi-cursor (Ctrl+N)
+
+  -- Simple opts-only plugins
+  { 'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+  { 'nvim-treesitter/nvim-treesitter-context', opts = { max_lines = 3 } },
+  { 'brenoprata10/nvim-highlight-colors',      opts = { render = 'background' } },
+  { 'Pocco81/auto-save.nvim',                  opts = {} },
+
+  -- ============================================================
+  -- [[ Colorscheme ]] (priority = 1000 so it loads first)
+  -- ============================================================
   {
+    'scottmckendry/cyberdream.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'cyberdream'
+    end,
+  },
+
+  -- ============================================================
+  -- [[ Advanced Configuration ]]
+  -- ============================================================
+
+  { -- Keybind hint popup
+    'folke/which-key.nvim',
+    event = 'VimEnter',
+    opts = {
+      delay = 0,
+      icons = {
+        mappings = vim.g.have_nerd_font,
+        keys = vim.g.have_nerd_font and {} or {
+          Up = '<Up> ', Down = '<Down> ', Left = '<Left> ', Right = '<Right> ',
+          C = '<C-…> ', M = '<M-…> ', D = '<D-…> ', S = '<S-…> ',
+          CR = '<CR> ', Esc = '<Esc> ', ScrollWheelDown = '<ScrollWheelDown> ',
+          ScrollWheelUp = '<ScrollWheelUp> ', NL = '<NL> ', BS = '<BS> ',
+          Space = '<Space> ', Tab = '<Tab> ',
+          F1 = '<F1>', F2 = '<F2>', F3 = '<F3>', F4 = '<F4>',
+          F5 = '<F5>', F6 = '<F6>', F7 = '<F7>', F8 = '<F8>',
+          F9 = '<F9>', F10 = '<F10>', F11 = '<F11>', F12 = '<F12>',
+        },
+      },
+      spec = {
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      },
+    },
+  },
+
+  { -- Neovim Lua API types & completions for Lua config files
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        { path = 'snacks.nvim',        words = { 'Snacks' } },
+      },
+    },
+  },
+
+  { -- LSP: server configs, Mason installer, diagnostic UI
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      { 'mason-org/mason.nvim', opts = {} },
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      { 'j-hui/fidget.nvim', opts = {} },
+      'saghen/blink.cmp',
+    },
+    config = function()
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+        callback = function(event)
+          local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
+
+          map('grn', vim.lsp.buf.rename,                              '[R]e[n]ame')
+          map('gra', vim.lsp.buf.code_action,                        '[G]oto Code [A]ction', { 'n', 'x' })
+          map('grr', require('snacks').picker.lsp_references,        '[G]oto [R]eferences')
+          map('gri', require('snacks').picker.lsp_implementations,   '[G]oto [I]mplementation')
+          map('grd', require('snacks').picker.lsp_definitions,       '[G]oto [D]efinition')
+          map('grD', vim.lsp.buf.declaration,                        '[G]oto [D]eclaration')
+          map('gO',  require('snacks').picker.lsp_symbols,           'Open Document Symbols')
+          map('gW',  require('snacks').picker.lsp_workspace_symbols, 'Open Workspace Symbols')
+          map('grt', require('snacks').picker.lsp_type_definitions,  '[G]oto [T]ype Definition')
+
+          ---@param client vim.lsp.Client
+          ---@param method vim.lsp.protocol.Method
+          ---@param bufnr? integer
+          ---@return boolean
+          local function client_supports_method(client, method, bufnr)
+            return client:supports_method(method, bufnr)
+          end
+
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+            local hl_group = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+              buffer = event.buf, group = hl_group, callback = vim.lsp.buf.document_highlight,
+            })
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+              buffer = event.buf, group = hl_group, callback = vim.lsp.buf.clear_references,
+            })
+            vim.api.nvim_create_autocmd('LspDetach', {
+              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+              end,
+            })
+          end
+
+          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+            map('<leader>th', function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+            end, '[T]oggle Inlay [H]ints')
+          end
+        end,
+      })
+
+      vim.diagnostic.config {
+        severity_sort = true,
+        float   = { border = 'rounded', source = 'if_many' },
+        underline = { severity = vim.diagnostic.severity.ERROR },
+        signs = vim.g.have_nerd_font and {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚 ',
+            [vim.diagnostic.severity.WARN]  = '󰀪 ',
+            [vim.diagnostic.severity.INFO]  = '󰋽 ',
+            [vim.diagnostic.severity.HINT]  = '󰌶 ',
+          },
+        } or {},
+        virtual_text = {
+          source = 'if_many', spacing = 2,
+          format = function(d) return d.message end,
+        },
+      }
+
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+      -- Add servers here. See :help lspconfig-all for the full list.
+      local servers = {
+        -- clangd = {}, gopls = {}, pyright = {}, rust_analyzer = {}, ts_ls = {}
+        lua_ls = {
+          settings = { Lua = { completion = { callSnippet = 'Replace' } } },
+        },
+      }
+
+      local lsp_to_mason = {
+        lua_ls = 'lua-language-server',
+        -- pyright = 'pyright', ts_ls = 'typescript-language-server',
+      }
+      local ensure_installed = vim.tbl_keys(servers or {})
+      for i, name in ipairs(ensure_installed) do
+        if lsp_to_mason[name] then ensure_installed[i] = lsp_to_mason[name] end
+      end
+      vim.list_extend(ensure_installed, { 'stylua' })
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+      for name, cfg in pairs(servers) do
+        local server = vim.tbl_deep_extend('force', {}, cfg)
+        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+        vim.lsp.config[name] = server
+        vim.lsp.enable(name)
+      end
+    end,
+  },
+
+  { -- Autoformat on save; <leader>f to format manually
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd   = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function() require('conform').format { async = true, lsp_format = 'fallback' } end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then return nil end
+        return { timeout_ms = 500, lsp_format = 'fallback' }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- python     = { 'isort', 'black' },
+        -- javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      },
+    },
+  },
+
+  { -- Completion engine with Copilot + LuaSnip sources
+    'saghen/blink.cmp',
+    event   = 'VimEnter',
+    version = '1.*',
+    dependencies = {
+      'giuxtaposition/blink-cmp-copilot',
+      {
+        'L3MON4D3/LuaSnip',
+        version = '2.*',
+        build = (function()
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then return end
+          return 'make install_jsregexp'
+        end)(),
+        opts = {},
+      },
+      'folke/lazydev.nvim',
+    },
+    --- @module 'blink.cmp'
+    --- @type blink.cmp.Config
+    opts = {
+      keymap    = { preset = 'default' },
+      appearance = { nerd_font_variant = 'mono' },
+      completion = { documentation = { auto_show = false, auto_show_delay_ms = 500 } },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'lazydev', 'copilot' },
+        providers = {
+          copilot  = { name = 'copilot',  module = 'blink-cmp-copilot',          score_offset = 100 },
+          lazydev  = {                    module = 'lazydev.integrations.blink',  score_offset = 100 },
+        },
+      },
+      snippets  = { preset = 'luasnip' },
+      fuzzy     = { implementation = 'prefer_rust_with_warning' },
+      signature = { enabled = true },
+    },
+  },
+
+  { -- mini.ai (text objects), mini.surround, mini.statusline
+    'echasnovski/mini.nvim',
+    config = function()
+      require('mini.ai').setup { n_lines = 500 }
+      require('mini.surround').setup()
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function() return '%2l:%-2v' end
+    end,
+  },
+
+  { -- Syntax highlighting, code-aware folding, text objects
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main  = 'nvim-treesitter.configs',
+    opts  = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      auto_install = true,
+      highlight    = { enable = true, additional_vim_regex_highlighting = { 'ruby' } },
+      indent       = { enable = true, disable = { 'ruby' } },
+    },
+  },
+
+  { -- File manager as a buffer (edit filesystem like text); `-` to open
     'stevearc/oil.nvim',
     dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     config = function()
@@ -16,169 +284,118 @@ return {
           ['<C-j>'] = false,
           ['<M-h>'] = 'actions.select_split',
         },
-        view_options = {
-          show_hidden = true,
-        },
+        view_options = { show_hidden = true },
       }
-
-      vim.keymap.set('n', '-', oil.open, { desc = 'Open parent directory with oil' })
-      vim.keymap.set('n', '<leader>o', oil.toggle_float)
+      vim.keymap.set('n', '-',          oil.open,        { desc = 'Open parent directory (oil)' })
+      vim.keymap.set('n', '<leader>o',  oil.toggle_float, { desc = 'Toggle oil float' })
     end,
   },
-  'lunarvim/synthwave84.nvim',
-  'emacs-grammarly/lsp-grammarly',
-  -- vim-illuminate and true-zen = :lua Snacks.zen() I dont actually hate it for writing.
-  -- Titus Custom
-  {
+
+  { -- Paste images from clipboard into Markdown (<leader>p)
     'HakonHarnes/img-clip.nvim',
     event = 'VeryLazy',
     opts = function()
       local year = os.date '%Y'
       return {
         default = {
-          dir_path = '/home/titus/github/website/static/images/' .. year .. '/',
-          extension = 'webp',
-          process_cmd = 'cwebp -q 80 "$FILE_PATH" -o "$FILE_PATH" 2>/dev/null',
-          template = '![$FILE_NAME_NO_EXT](/images/' .. year .. '/$FILE_NAME)',
+          dir_path           = '/home/titus/github/website/static/images/' .. year .. '/',
+          extension          = 'webp',
+          process_cmd        = 'cwebp -q 80 "$FILE_PATH" -o "$FILE_PATH" 2>/dev/null',
+          template           = '![$FILE_NAME_NO_EXT](/images/' .. year .. '/$FILE_NAME)',
           relative_template_path = false,
         },
         filetypes = {
-          markdown = {
-            template = '![$FILE_NAME_NO_EXT](/images/' .. year .. '/$FILE_NAME)',
-          },
-        },
-      }
-    end,
-  },
-  'mbbill/undotree',
-  'wakatime/vim-wakatime',
-  {
-    'Pocco81/auto-save.nvim',
-    config = function()
-      require('auto-save').setup {
-        -- your config goes here
-        -- or just leave it empty :)
-      }
-    end,
-  },
-  'lambdalisue/suda.vim',
-  --  'github/copilot.vim',
-  {
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end,
-  },
-  'ionide/Ionide-vim',
-  {
-    'greggh/claude-code.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim', -- Required for git operations
-    },
-    config = function()
-      require('claude-code').setup()
-    end,
-  },
-  {
-    'goolord/alpha-nvim',
-    -- dependencies = { 'echasnovski/mini.icons' },
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      local startify = require 'alpha.themes.startify'
-      -- available: devicons, mini, default is mini
-      -- if provider not loaded and enabled is true, it will try to use another provider
-      startify.file_icons.provider = 'devicons'
-      require('alpha').setup(startify.config)
-    end,
-  },
-  {
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    event = 'InsertEnter',
-    config = function()
-      require('copilot').setup {
-        suggestion = { enabled = false }, -- Disable suggestions since we're using blink.cmp
-        panel = { enabled = false },      -- Disable panel since we're using blink.cmp
-        filetypes = {
-          markdown = true,
-          yaml = true,
-          help = false,
-          gitcommit = false,
-          gitrebase = false,
-          hgcommit = false,
-          svn = false,
-          cvs = false,
-          ["."] = false,
+          markdown = { template = '![$FILE_NAME_NO_EXT](/images/' .. year .. '/$FILE_NAME)' },
         },
       }
     end,
   },
 
-  -- VSCode-style tab bar
-  {
+  { -- Smart comment toggling (<leader>/)
+    'numToStr/Comment.nvim',
+    config = function() require('Comment').setup() end,
+  },
+
+  { -- Claude AI chat integration (<leader>cc)
+    'greggh/claude-code.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function() require('claude-code').setup() end,
+  },
+
+  { -- Dashboard / start screen (<leader>a)
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local startify = require 'alpha.themes.startify'
+      startify.file_icons.provider = 'devicons'
+      require('alpha').setup(startify.config)
+    end,
+  },
+
+  { -- GitHub Copilot (suggestions fed through blink.cmp, not inline)
+    'zbirenbaum/copilot.lua',
+    cmd   = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        suggestion = { enabled = false },
+        panel      = { enabled = false },
+        filetypes  = {
+          markdown  = true,
+          yaml      = true,
+          help      = false, gitcommit = false, gitrebase = false,
+          hgcommit  = false, svn = false, cvs = false, ['.'] = false,
+        },
+      }
+    end,
+  },
+
+  { -- VSCode-style buffer tab bar
     'akinsho/bufferline.nvim',
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
     opts = {
       options = {
-        mode = 'buffers',
+        mode        = 'buffers',
         diagnostics = 'nvim_lsp',
-        offsets = { { filetype = 'snacks_layout_box', text = 'Explorer' } },
+        offsets     = { { filetype = 'snacks_layout_box', text = 'Explorer' } },
       },
     },
     keys = {
-      { '<S-l>', '<cmd>BufferLineCycleNext<CR>', desc = 'Next buffer' },
-      { '<S-h>', '<cmd>BufferLineCyclePrev<CR>', desc = 'Prev buffer' },
-      { '<leader>x', '<cmd>bdelete<CR>', desc = 'Close buffer' },
+      { '<S-l>',      '<cmd>BufferLineCycleNext<CR>', desc = 'Next buffer' },
+      { '<S-h>',      '<cmd>BufferLineCyclePrev<CR>', desc = 'Prev buffer' },
+      { '<leader>x',  '<cmd>bdelete<CR>',             desc = 'Close buffer' },
     },
   },
 
-  -- Integrated terminal (Ctrl+` like VSCode)
-  {
+  { -- Integrated terminal toggled with Ctrl+` (like VSCode)
     'akinsho/toggleterm.nvim',
     version = '*',
     opts = {
-      size = 15,
+      size         = 15,
       open_mapping = [[<C-`>]],
-      direction = 'horizontal',
+      direction    = 'horizontal',
       shade_terminals = true,
     },
   },
 
-  -- Problems panel (like VSCode Ctrl+Shift+M)
-  {
+  { -- Problems panel (<leader>xx), like VSCode Ctrl+Shift+M
     'folke/trouble.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
     keys = {
-      { '<leader>xx', '<cmd>Trouble diagnostics toggle<CR>', desc = 'Diagnostics (Trouble)' },
-      { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<CR>', desc = 'Buffer Diagnostics' },
-      { '<leader>cs', '<cmd>Trouble symbols toggle<CR>', desc = 'Symbols (Trouble)' },
+      { '<leader>xx', '<cmd>Trouble diagnostics toggle<CR>',               desc = 'Diagnostics (Trouble)' },
+      { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<CR>',  desc = 'Buffer Diagnostics' },
+      { '<leader>cs', '<cmd>Trouble symbols toggle<CR>',                   desc = 'Symbols (Trouble)' },
     },
     opts = {},
   },
 
-  -- Sticky scroll (shows current function/class at top like VSCode)
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    opts = { max_lines = 3 },
-  },
-
-  -- Symbol outline sidebar (like VSCode outline panel)
-  {
+  { -- Symbol outline sidebar, like VSCode outline panel (<leader>ao)
     'stevearc/aerial.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     keys = {
       { '<leader>ao', '<cmd>AerialToggle!<CR>', desc = 'Toggle [A]erial [O]utline' },
     },
     opts = {},
-  },
-
-  -- Multi-cursor (Ctrl+N to select next, like VSCode Ctrl+D)
-  { 'mg979/vim-visual-multi', branch = 'master' },
-
-  -- Inline color swatches (#fff, rgb(), etc.)
-  {
-    'brenoprata10/nvim-highlight-colors',
-    opts = { render = 'background' },
   },
 }
--- titus-kickstart/lua/keymaps.lua
+++ titus-kickstart/lua/keymaps.lua
@@ -1,73 +1,71 @@
--- Shorten function name
-local keymap = vim.keymap.set
--- Silent keymap option
+local map = vim.keymap.set
 local opts = { silent = true }
 
---Remap space as leader key
-keymap('', '<Space>', '<Nop>', opts)
-vim.g.mapleader = ' '
+-- ============================================================
+-- [[ Window Navigation & Resize ]]
+-- ============================================================
+map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus left' })
+map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus down' })
+map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus up' })
+map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus right' })
+map('n', '<C-Up>',    ':resize -2<CR>',          opts)
+map('n', '<C-Down>',  ':resize +2<CR>',          opts)
+map('n', '<C-Left>',  ':vertical resize -2<CR>', opts)
+map('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
--- Modes
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
+-- ============================================================
+-- [[ Buffer & Tab Navigation ]]
+-- ============================================================
+-- <S-h>/<S-l> are overridden by bufferline.nvim with BufferLineCyclePrev/Next
+map('n', '<S-h>', ':bprevious<CR>', opts)
+map('n', '<S-l>', ':bnext<CR>',     opts)
+map('n', '<S-q>', '<cmd>bdelete!<CR>', { silent = true, desc = 'Close buffer' })
 
--- Normal --
--- Better window navigation
-keymap('n', '<C-h>', '<C-w>h', opts)
-keymap('n', '<C-j>', '<C-w>j', opts)
-keymap('n', '<C-k>', '<C-w>k', opts)
-keymap('n', '<C-l>', '<C-w>l', opts)
+map('n', '<Leader>1', '1gt', { silent = true, desc = 'Go to tab 1' })
+map('n', '<Leader>2', '2gt', { silent = true, desc = 'Go to tab 2' })
+map('n', '<Leader>3', '3gt', { silent = true, desc = 'Go to tab 3' })
+map('n', '<Leader>4', '4gt', { silent = true, desc = 'Go to tab 4' })
+map('n', '<Leader>5', '5gt', { silent = true, desc = 'Go to tab 5' })
+map('n', '<Leader>t', '<cmd>tabnew<CR>',   { silent = true, desc = 'New tab' })
+map('n', '<Leader>c', '<cmd>tabclose<CR>', { silent = true, desc = 'Close tab' })
 
--- Resize with arrows
-keymap('n', '<C-Up>', ':resize -2<CR>', opts)
-keymap('n', '<C-Down>', ':resize +2<CR>', opts)
-keymap('n', '<C-Left>', ':vertical resize -2<CR>', opts)
-keymap('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+-- ============================================================
+-- [[ Search ]]
+-- ============================================================
+map('n', '<Esc>',      '<cmd>nohlsearch<CR>',  { desc = 'Clear search highlights' })
+map('n', '<leader>nh', '<cmd>nohlsearch<CR>',  { desc = '[N]o [H]ighlight — clear search' })
 
--- Navigate buffers
-keymap('n', '<S-l>', ':bnext<CR>', opts)
-keymap('n', '<S-h>', ':bprevious<CR>', opts)
+-- ============================================================
+-- [[ Diagnostics ]]
+-- ============================================================
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Clear highlights
-keymap('n', '<leader>h', '<cmd>nohlsearch<CR>', opts)
+-- ============================================================
+-- [[ Terminal ]]
+-- ============================================================
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- Close buffers
-keymap('n', '<S-q>', '<cmd>Bdelete!<CR>', opts)
+-- ============================================================
+-- [[ Editing ]]
+-- ============================================================
+map('i', 'jj', '<ESC>',   { silent = true, desc = 'Exit insert mode' })
+map('v', 'p',  '"_dP',    { silent = true, desc = 'Paste without overwriting clipboard' })
+map('v', '<',  '<gv',     { silent = true, desc = 'Dedent and stay in visual mode' })
+map('v', '>',  '>gv',     { silent = true, desc = 'Indent and stay in visual mode' })
+map('n', '<leader>e', '$', { silent = true, desc = 'Jump to [E]nd of line' })
+map('n', 'S', ':%s//g<Left><Left>', { desc = 'Search and replace in buffer' })
 
--- Better paste
-keymap('v', 'p', '"_dP', opts)
+-- ============================================================
+-- [[ Comments ]]
+-- ============================================================
+map('n', '<leader>/', "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
+map('x', '<leader>/', '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>')
 
--- Insert --
--- Press jj fast to enter
-keymap('i', 'jj', '<ESC>', opts)
-
--- Visual --
--- Stay in indent mode
-keymap('v', '<', '<gv', opts)
-keymap('v', '>', '>gv', opts)
-
--- Plugins --
-keymap('n', '<leader>a', ':Alpha<CR>', opts)
-keymap('n', '<leader>cc', ':ClaudeCode<CR>', opts)
-
--- Comment
-keymap('n', '<leader>/', "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
-keymap('x', '<leader>/', '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>')
-
--- Custom
-keymap('n', '<leader>p', '<cmd> PasteImage <CR>', opts)
-keymap('n', '<leader>e', '$', opts)
-keymap('n', 'S', '<cmd> %s//g', opts)
-keymap('n', '<F5>', '<cmd> UndotreeToggle <CR> <cmd> UndotreeFocus <CR>', opts)
-keymap('n', '<C-\\>', '<cmd> TZAtaraxis <CR>', opts)
-keymap('n', '<Leader>1', '1gt<CR>', opts)
-keymap('n', '<Leader>2', '2gt<CR>', opts)
-keymap('n', '<Leader>3', '3gt<CR>', opts)
-keymap('n', '<Leader>4', '4gt<CR>', opts)
-keymap('n', '<Leader>5', '5gt<CR>', opts)
-keymap('n', '<Leader>t', '<cmd> tabnew<CR>', opts)
-keymap('n', '<Leader>c', '<cmd> tabclose<CR>', opts)
+-- ============================================================
+-- [[ Tools & Plugins ]]
+-- ============================================================
+map('n', '<leader>a',  ':Alpha<CR>',                                    { silent = true, desc = 'Open [A]lpha dashboard' })
+map('n', '<leader>cc', ':ClaudeCode<CR>',                               { silent = true, desc = '[C]laude [C]ode' })
+map('n', '<leader>p',  '<cmd>PasteImage<CR>',                           { silent = true, desc = '[P]aste image from clipboard' })
+map('n', '<F5>',       '<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>', { silent = true, desc = 'Toggle Undotree' })
+map('n', '<C-\\>',   '<cmd>TZAtaraxis<CR>',                           { silent = true, desc = 'Toggle Zen mode' })
