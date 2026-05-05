9c8a6e3b805ecbe2746aa70f9cffed1bfd33c7cb
Chris Titus <contact@christitus.com>
2026-05-04 21:17:55 -0500

    color

--- titus-kickstart/plugin/00-colorscheme.lua
+++ titus-kickstart/plugin/00-colorscheme.lua
@@ -1,44 +1,47 @@
 -- Colorscheme: loaded first (00- prefix) to avoid flash of default colors.
-vim.pack.add({
+vim.pack.add {
   'https://github.com/rebelot/kanagawa.nvim',
   'https://github.com/lunarvim/synthwave84.nvim', -- alternate colorscheme
-})
+}
 
 require('kanagawa').setup {
   overrides = function(colors)
     local theme = colors.theme
     return {
-      -- Make floating windows stand out
-      NormalFloat = { bg = theme.ui.bg_dim },
-      FloatBorder = { fg = theme.ui.special, bg = theme.ui.bg_dim },
-      FloatTitle = { fg = theme.ui.special, bold = true },
+      -- Floats
+      NormalFloat = { fg = theme.ui.fg, bg = theme.ui.bg_m3 },
+      FloatBorder = { fg = theme.ui.special, bg = theme.ui.bg_m3 },
+      FloatTitle = { fg = theme.ui.special, bg = theme.ui.bg_m3, bold = true },
 
-      -- Bolder keywords and types
+      -- Syntax: less blanket bold, more hierarchy
       ['@keyword'] = { fg = theme.syn.keyword, bold = true },
-      ['@type.builtin'] = { fg = theme.syn.type, bold = true },
+      ['@type.builtin'] = { fg = theme.syn.type },
       ['@function'] = { fg = theme.syn.fun, bold = true },
       ['@constructor'] = { fg = theme.syn.fun },
-
-      -- Make strings and comments more distinct
-      ['@string'] = { fg = theme.syn.string, italic = true },
+      ['@string'] = { fg = theme.syn.string },
       Comment = { fg = theme.syn.comment, italic = true },
-
-      -- Brighten constants and numbers
-      ['@constant'] = { fg = theme.syn.constant, bold = true },
-      ['@number'] = { fg = theme.syn.number, bold = true },
+      ['@constant'] = { fg = theme.syn.constant },
+      ['@number'] = { fg = theme.syn.number },
 
       -- Popup menu
-      Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-      PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
+      Pmenu = { fg = theme.ui.fg, bg = theme.ui.bg_p1 },
+      PmenuSel = { fg = theme.ui.fg, bg = theme.ui.bg_p2, bold = true },
       PmenuSbar = { bg = theme.ui.bg_m1 },
       PmenuThumb = { bg = theme.ui.special },
 
-      -- Make telescope pop
+      -- Telescope
       TelescopeTitle = { fg = theme.ui.special, bold = true },
-      TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+      TelescopePromptNormal = { fg = theme.ui.fg, bg = theme.ui.bg_p1 },
       TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-      TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-      TelescopeBorderNormal = { fg = theme.ui.bg_p2, bg = theme.ui.bg_m1 },
+      TelescopePreviewNormal = { fg = theme.ui.fg, bg = theme.ui.bg_dim },
+      TelescopeBorderNormal = { fg = theme.ui.special, bg = theme.ui.bg_m1 },
+
+      -- Extra pop
+      Visual = { bg = theme.ui.bg_p2 },
+      Search = { fg = theme.ui.bg, bg = theme.syn.fun, bold = true },
+      IncSearch = { fg = theme.ui.bg, bg = theme.syn.keyword, bold = true },
+      CursorLine = { bg = theme.ui.bg_m1 },
+      WinSeparator = { fg = theme.ui.special },
     }
   end,
 }
